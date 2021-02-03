4427f66ad2e46614f87705fbce5965a9bf878a77
yutonishi <yuto.nishi10@gmail.com>
2021-02-03 14:01:08 +0900

    Open Weather Map API読み込み完了

--- app.js
+++ app.js
@@ -12,7 +12,28 @@ const items = [
     el: '#app',
     data: {
       items: items,
-      searchPrefecture: '' //入力された県名を格納
+      searchPrefecture: '', //入力された県名を格納
+      city: null,
+      temp: null,
+      condition: {
+        main: null
+      }
+    },
+    mounted: function(){
+      axios.get('https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=0df130b4cca7062f4bd8ec6b62fcdcc2')
+      .then(function(response){
+        this.city = response.data.name
+        this.temp = response.data.main.temp
+        this.condition = response.data.weather[0]
+      }.bind(this))
+      .catch(function(error){
+        console.log(error)
+      })
+    },
+    filters: {
+      roundUp(value){
+        return Math.ceil(value)
+      }
     },
     computed: {
       eventedSearch: function(){
