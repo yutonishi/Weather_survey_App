fd970f8c40c273e2b56ec9be0a5bb3424ff11bef
yutonishi <yuto.nishi10@gmail.com>
2021-02-09 20:42:07 +0900

    クリックで県ごとの天気表示（桑名さん）

--- app.js
+++ app.js
@@ -1,77 +1,56 @@
+// const items = [
+//     {'Id':'1', 'Name':'tokyo'},
+//     {'Id':'2', 'Name':'chiba'},
+//     {'Id':'3', 'Name':'saitama'},
+//   ];
+
 const items = [
-    {'Id':'1', 'Name':'tokyo'},
-    {'Id':'2', 'Name':'chiba'},
-    {'Id':'3', 'Name':'saitama'},
-    {'Id':'4', 'Name':'kanagawa'},
-    {'Id':'5', 'Name':'ibaragi'},
-    {'Id':'6', 'Name':'gunma'},
-    {'Id':'7', 'Name':'tochigi'}
-  ];
+  {'Id':'1', 'Roman':'Tokyo', 'Name':'東京都'},
+  {'Id':'2', 'Roman':'Chiba', 'Name':'千葉県'},
+  {'Id':'3', 'Roman':'Kanagawa', 'Name':'神奈川県'},
+  {'Id':'4', 'Roman':'Saitama', 'Name':'埼玉県'},
+  {'Id':'5', 'Roman':'Ibaragi', 'Name':'茨城県'},
+  {'Id':'6', 'Roman':'Gunma', 'Name':'郡馬県'},
+  {'Id':'7', 'Roman':'Tochigi', 'Name':'栃木県'}
+]
 
   var app = new Vue ({
     el: '#app',
     data: {
       items: items,
-      searchPrefecture: '', //入力された県名を格納
-      searchRegion: '',
-      prefs: '',
-      // code: '',
-      // name: '',
+      requestPref: '',
       city: null,
       temp: null,
       condition: {
         main: null
       }
     },
-    beforeUpdate: function(){//mountedだとエラーになる（ライフサイクルについて）
-      axios.get('https://api.openweathermap.org/data/2.5/weather', {
-        params: { q: this.searchRegion, APPID: '0df130b4cca7062f4bd8ec6b62fcdcc2'}
-      })
-      .then(function(response){
-        this.city = response.data.name
-        this.temp = response.data.main.temp
-        this.condition = response.data.weather[0]
-      }.bind(this))
-      .catch(function(error){
-        console.log(error)
-      })
+    methods: {
+      pref: function(pref){
+        this.requestPref = pref;
+        console.log(pref)
+        this.getData()
+      },
+      getData: function() {
+        axios.get('https://api.openweathermap.org/data/2.5/weather', {
+          params: { q: this.requestPref, APPID: '0df130b4cca7062f4bd8ec6b62fcdcc2'}
+        })
+        .then(function(response){
+          this.city = response.data.name
+          this.temp = response.data.main.temp
+          this.condition = response.data.weather[0]
+        }.bind(this))
+        .catch(function(error){
+          console.log(error)
+        })  
+      }
     },
-    mounted: function(){
-      axios.get('https://opendata.resas-portal.go.jp/api/v1/prefectures',{
-        headers: {
-          "Content-Type": "application/json",
-          "X-API-KEY": "ZTeuqJL7rj5gSslTavfjvbmV83NSvTvFUjQsNdY8"
-        }
-      })
-      // .then(function(response){
-      //   this.code = response.data.result.prefCode
-      //   this.name = response.data.result.prefName
-      // })
-      .then(response => this.prefs = response.data.result)
-      .catch(function(error){
-        console.log(error)
-      })
+    beforeUpdate: function(){//mountedだとエラーになる（ライフサイクルについて）
+      // this.getData()
     },
     filters: {
       roundUp(value){
         return Math.ceil(value)
       }
-    },
-    //検索機能
-    computed: {
-      eventedSearch: function(){
-        let list = this.items.slice();
-
-        if(this.searchPrefecture){
-          list = list.filter(element => {
-            return Object.keys(element).some(key => {
-              if(key === 'Name'){
-                return element[key].indexOf(this.searchPrefecture) > -1;
-              }
-            });
-          });
-        }
-        return list;
-      }
-    },
+    }
   })
